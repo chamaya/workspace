141144516ae104fdd3ca344af458561e6cd7ec29
CAmaya <camaya@dealersocket.com>
2018-09-05 12:51:24 -0700

    Adding Moves

--- Android App/PokemonTeamBuilder/app/src/main/java/com/example/camaya/pokemonteambuilder/PokemonCardFragment.java
+++ Android App/PokemonTeamBuilder/app/src/main/java/com/example/camaya/pokemonteambuilder/PokemonCardFragment.java
@@ -1,24 +1,34 @@
 package com.example.camaya.pokemonteambuilder;
 
 
+import android.content.res.Resources;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.res.ResourcesCompat;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import com.koushikdutta.async.future.Future;
 import com.koushikdutta.async.future.FutureCallback;
 import com.koushikdutta.ion.Ion;
 import com.squareup.picasso.Picasso;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.w3c.dom.Text;
+
+import java.util.HashMap;
+import java.util.concurrent.CancellationException;
+import java.util.concurrent.ThreadLocalRandom;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -35,11 +45,42 @@ public class PokemonCardFragment extends Fragment {
     ImageView PokemonImage;
     @BindView(R.id.pokemon_name)
     TextView PokemonName;
+    @BindView (R.id.pokemon_type)
+    TextView PokemonType;
+    @BindView(R.id.pokemon_moves)
+    TextView PokemonMoves;
+    @BindView(R.id.pokemon_details_layout)
+    LinearLayout PokemonDetailsLayout;
+
 
     int PokemonId;
+    Future<String> PokemonNameThread;
 
     final String POKEMON_IMAGE_URL = "https://www.serebii.net/art/th/%d.png";
     final String POKEMON_INFO_URL = "https://pokeapi.co/api/v2/pokemon/%d/";
+    final int NUMBER_OF_POKEMON_MOVES = 4;
+    final HashMap<String, Integer> POKEMON_TYPE_COLORS = new HashMap<String, Integer>(){
+        {
+            put("Normal", R.color.NormalType);
+            put("Fire", R.color.FireType);
+            put("Water", R.color.WaterType);
+            put("Electric", R.color.ElectricType);
+            put("Grass", R.color.GrassType);
+            put("Ice", R.color.IceType);
+            put("Fighting", R.color.FightingType);
+            put("Poison", R.color.PoisonType);
+            put("Ground", R.color.GroundType);
+            put("Flying", R.color.FlyingType);
+            put("Psychic", R.color.PsychicType);
+            put("Bug", R.color.BugType);
+            put("Rock", R.color.RockType);
+            put("Ghost", R.color.GhostType);
+            put("Dragon", R.color.DragonType);
+            put("Dark", R.color.DarkType);
+            put("Steel", R.color.SteelType);
+            put("Fairy", R.color.FairyType);
+        }
+    };
 
     public PokemonCardFragment() {
         // Required empty public constructor
@@ -64,7 +105,7 @@ public class PokemonCardFragment extends Fragment {
     public void onViewStateRestored(@Nullable Bundle savedInstanceState) {
         super.onViewStateRestored(savedInstanceState);
         if(savedInstanceState == null){
-            PokemonId = 25;
+            PokemonId = 1;
         }
         else{
             PokemonId = savedInstanceState.getInt("PokemonId", 25);
@@ -80,33 +121,94 @@ public class PokemonCardFragment extends Fragment {
     }
 
     public void updateCardById(int id){
+        if(PokemonNameThread != null){
+            PokemonNameThread.cancel(true);
+        }
+        resetLayout();
         PokemonId = id;
+
         final String pokemonImageUrlFormated = String.format(POKEMON_IMAGE_URL, PokemonId);
         Picasso.get()
                 .load(pokemonImageUrlFormated)
-                .resize(1000,1000)
-                .placeholder(R.drawable.mystery)
+                .placeholder(R.drawable.pikachu_sill)
                 .into(PokemonImage);
 
         final String pokemonInfoUrlFormated = String.format(POKEMON_INFO_URL, PokemonId);
-        Ion.with(this)
+        PokemonNameThread = Ion.with(this)
                 .load(pokemonInfoUrlFormated)
                 .asString()
                 .setCallback(new FutureCallback<String>() {
                     @Override
                     public void onCompleted(Exception e, String result) {
                         try{
+                            if(e instanceof CancellationException){
+                                throw new CancellationException();
+                            }
                             JSONObject pokemonJson = new JSONObject(result);
-                            String pokemonName = pokemonJson.getJSONObject("species").getString("name");
-                            pokemonName = pokemonName.substring(0,1).toUpperCase() +
-                                    pokemonName.substring(1);
-                            PokemonName.setText(pokemonName);
+                            setLayout(pokemonJson);
 
-                        }catch(JSONException je){
+                        }catch(JSONException je) {
                             Log.v("HELP", pokemonInfoUrlFormated);
+                        }catch(CancellationException ce){
+                            return;
                         }
                     }
+
                 });
 
     }
+
+    private void resetLayout() {
+        PokemonName.setText("Loading...");
+        PokemonType.setText("...");
+        PokemonMoves.setText("...");
+
+    }
+
+    private void setLayout(JSONObject pokemonJson){
+        String pokemonName = "";
+        String pokemonType = "";
+        String pokemonMoves = "";
+        String mainType = "";
+        try {
+            pokemonName = pokemonJson.getJSONObject("species").getString("name");
+
+            JSONArray pokemonTypeArray;
+            pokemonTypeArray = pokemonJson.getJSONArray("types");
+            for(int type = 0; type < pokemonTypeArray.length(); type++){
+                pokemonType += pokemonTypeArray.getJSONObject(type).getJSONObject("type").getString("name") + "/";
+                if(pokemonTypeArray.getJSONObject(type).getInt("slot") == 1){
+                    mainType = pokemonType.split("/")[type];
+                    mainType = mainType.substring(0,1).toUpperCase() +
+                            mainType.substring(1);
+                }
+            }
+
+            JSONArray pokemonMovesArray = pokemonJson.getJSONArray("moves");
+            if(pokemonMovesArray.length() <= NUMBER_OF_POKEMON_MOVES){
+                for(int move = 0; move < pokemonMovesArray.length(); move++){
+                    pokemonMoves += pokemonMovesArray.getJSONObject(move).getJSONObject("move").getString("name") + "\n";
+                }
+            }else{
+                int move = ThreadLocalRandom.current().nextInt(pokemonMovesArray.length());
+                for(int c =0; c < NUMBER_OF_POKEMON_MOVES; c++,
+                    move = (move + 1) % pokemonMovesArray.length()){
+                    pokemonMoves += pokemonMovesArray.getJSONObject(move).getJSONObject("move").getString("name") + "\n";
+                }
+            }
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        pokemonName = pokemonName.substring(0,1).toUpperCase() +
+                pokemonName.substring(1);
+        PokemonName.setText(pokemonName);
+        PokemonType.setText(pokemonType.substring(0,pokemonType.length() - 1));
+        PokemonMoves.setText(pokemonMoves.substring(0,pokemonMoves.length() - 1));
+        int color = POKEMON_TYPE_COLORS.get(mainType);
+        PokemonDetailsLayout.setBackgroundColor(getResources().getColor(color));
+
+
+    }
 }
+//java.util.concurrent.CancellationException
